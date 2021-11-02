2195e2912a78202988b3cc0f4e662bed8421bf48
Evan Mitchell <etmitchell@bsu.edu>
2021-11-01 21:33:55 -0400

    Add custom collision zone for the car along with sleeping state check

--- src/Car/Car.gd
+++ src/Car/Car.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-export var SPEED = 60000
+export var SPEED = 40000
 export var MASS = 300
 export var WEIGHT = 2940
 
@@ -12,17 +12,17 @@ func _ready() -> void:
 	pass
 	
 func _process(_delta):
-	pass
+	if car.sleeping:
+		print("sleeping")
 
 func _physics_process(delta) -> void:
 	if Input.is_action_pressed("forwards"):
-		apply_torque_impulse(-6000 * delta * 60)
+		apply_torque_impulse(6000 * delta * 100)
 		rightWheel.apply_torque_impulse(SPEED * delta * 60)
 		leftWheel.apply_torque_impulse(SPEED * delta * 60)
 
 	elif Input.is_action_pressed("backwards"):
-		apply_torque_impulse(2000 * delta * 60)
+		apply_torque_impulse(-6000 * delta * 100)
 		rightWheel.apply_torque_impulse(-SPEED * delta * 60)
 		leftWheel.apply_torque_impulse(-SPEED * delta * 60)
 		
-
