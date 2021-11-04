af0f6eb04d57c62e971a6b2191991537c75405b0
Evan Mitchell <etmitchell@bsu.edu>
2021-11-04 09:35:54 -0400

    Rename common assets folder

--- src/Autoloads/MusicController.gd
+++ src/Autoloads/MusicController.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var game_music := load("res://src/Common/backgroundMusic.ogg")
+var game_music := load("res://src/CommonAssets/backgroundMusic.ogg")
 	
 func play_music() -> void:
 	$Music.stream = game_music
