0f03cf1821cffd649f3db0b26e019deb94e5a7d0
Evan Mitchell <etmitchell@bsu.edu>
2021-11-03 22:52:49 -0400

    Change background music file type to .ogg

--- src/Autoloads/MusicController.gd
+++ src/Autoloads/MusicController.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var game_music := load("res://src/Common/backgroundMusic.wav")
+var game_music := load("res://src/Common/backgroundMusic.ogg")
 	
 func play_music() -> void:
 	$Music.stream = game_music
