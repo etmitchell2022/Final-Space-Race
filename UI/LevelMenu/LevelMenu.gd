583e2b42e64b7309a984fd8e2270391681a08a5d
carterj <cdjeurissen@bsu.edu>
2021-11-20 19:10:10 -0500

    Add level menu

---
+++ UI/LevelMenu/LevelMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_LevelOneButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_LevelTwoButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_LevelThreeButton_pressed():
+	pass # Replace with function body.
