79fdb7f18396650dde79c6539bbd39a339fe9eaf
carterj <cdjeurissen@bsu.edu>
2021-11-22 21:54:07 -0500

    Add explosions to all vehicles

--- Vehicles/Truck/Truck.gd
+++ Vehicles/Truck/Truck.gd
@@ -1,16 +1,27 @@
 extends "res://Vehicles/Car.gd"
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var crashes = 0
+onready var deathTimer = $DeathTimer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_CrashZone_body_entered(body):
+	if crashes == 1:
+		if body != RigidBody2D:
+			$Sprite.visible = false
+			$LeftWheel.visible = false
+			$RightWheel.visible = false
+			var explosion : CPUParticles2D = load("res://Autoloads/Explosion.tscn").instance()
+			explosion.one_shot = true
+			explosion.position = $CrashZone.position
+			explosion.modulate = Color(48, 125, 15)
+			print(explosion)
+			call_deferred("add_child", explosion)
+			car.MODE_STATIC
+			$CrashSound.play()
+			deathTimer.start()
+	crashes += 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_DeathTimer_timeout():
+	get_tree().change_scene("res://UI/End/End.tscn")
--- Vehicles/YellowCar/YellowCar.gd
+++ Vehicles/YellowCar/YellowCar.gd
@@ -1,5 +1,27 @@
 extends "res://Vehicles/Car.gd"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+var crashes = 0
+onready var deathTimer = $DeathTimer
+
+
+func _on_CrashZone_body_entered(body):
+	if crashes == 1:
+		if body != RigidBody2D:
+			$Sprite.visible = false
+			$LeftWheel.visible = false
+			$RightWheel.visible = false
+			var explosion : CPUParticles2D = load("res://Autoloads/Explosion.tscn").instance()
+			explosion.one_shot = true
+			explosion.position = $CrashZone.position
+			explosion.modulate = Color(251, 229, 8)
+			print(explosion)
+			call_deferred("add_child", explosion)
+			car.MODE_STATIC
+			$CrashSound.play()
+			deathTimer.start()
+	crashes += 1
+
+
+func _on_DeathTimer_timeout():
+	get_tree().change_scene("res://UI/End/End.tscn")
