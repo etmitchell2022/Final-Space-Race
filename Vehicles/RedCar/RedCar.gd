31a55e0349cc1c0a807710e186aeb6c8a1ca9306
carterj <cdjeurissen@bsu.edu>
2021-11-22 21:29:48 -0500

    Add effects to explosion

--- Vehicles/RedCar/RedCar.gd
+++ Vehicles/RedCar/RedCar.gd
@@ -2,18 +2,24 @@ extends "res://Vehicles/Car.gd"
 
 
 var crashes = 0
+onready var deathTimer = $DeathTimer
 
 
 func _on_CrashZone_body_entered(body):
-	if crashes > 0:
+	if crashes == 1:
 		if body != RigidBody2D:
 			$Sprite.visible = false
 			$LeftWheel.visible = false
 			$RightWheel.visible = false
 			var explosion : CPUParticles2D = load("res://Autoloads/Explosion.tscn").instance()
 			explosion.one_shot = true
-			print(explosion)
 			explosion.position = $CrashZone.position
-			explosion.visible = true
 			call_deferred("add_child", explosion)
+			car.MODE_STATIC
+			$CrashSound.play()
+			deathTimer.start()
 	crashes += 1
+
+
+func _on_DeathTimer_timeout():
+	get_tree().change_scene("res://UI/End/End.tscn")
