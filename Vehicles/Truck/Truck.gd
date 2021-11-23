5ca0d9af4fa5541998f0eacb78556230c36cd55c
Evan Mitchell <etmitchell@bsu.edu>
2021-11-22 23:15:47 -0500

    Refactor vehicle scripts to inherit explosion funcion in parent car script

--- Vehicles/Truck/Truck.gd
+++ Vehicles/Truck/Truck.gd
@@ -1,27 +1,15 @@
 extends "res://Vehicles/Car.gd"
 
-
 var crashes = 0
 onready var deathTimer = $DeathTimer
-
 
 func _on_CrashZone_body_entered(body):
 	if crashes == 1:
 		if body != RigidBody2D:
-			$Sprite.visible = false
-			$LeftWheel.visible = false
-			$RightWheel.visible = false
-			var explosion : CPUParticles2D = load("res://Autoloads/Explosion.tscn").instance()
-			explosion.one_shot = true
-			explosion.position = $CrashZone.position
-			explosion.modulate = Color(48, 125, 15)
-			print(explosion)
-			call_deferred("add_child", explosion)
-			car.MODE_STATIC
-			$CrashSound.play()
+			._explosion()
 			deathTimer.start()
 	crashes += 1
 
 
 func _on_DeathTimer_timeout():
-	get_tree().change_scene("res://UI/End/End.tscn")
+	var _ignored := get_tree().change_scene("res://UI/End/End.tscn")
