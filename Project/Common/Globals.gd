789aa463eb0c526303a67ad913f89b6850fade8b
carterj <cdjeurissen@bsu.edu>
2021-11-30 14:11:08 -0500

    Add sun level to full project

--- Project/Common/Globals.gd
+++ Project/Common/Globals.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal new_max_speed(_max_speed)
 
+const SUN = "res://Project/Levels/SunLevel/Sun.tscn"
 const EARTH = "res://Project/Levels/EarthLevel/Earth.tscn"
 const MOON = "res://Project/Levels/MoonLevel/Moon.tscn"
 const MARS = "res://Project/Levels/MarsLevel/Mars.tscn"
@@ -47,6 +48,8 @@ func determine_current_level(scene) -> void:
 		previous_scene = MOON
 	elif scene == MARS:
 		previous_scene = MARS
+	elif scene == SUN:
+		previous_scene = SUN
 		
 func return_previous_scene() -> String:
 	return previous_scene
--- Project/Levels/SunLevel/Sun.gd
+++ Project/Levels/SunLevel/Sun.gd
@@ -1,6 +1,30 @@
 extends Node2D
 
+onready var SPAWN_POINT = $SpawnPoint.position
+onready var transition := $CanvasLayer/TransitonColor
+onready var hud := $CanvasLayer/HUD
 
+var vehicle
+
+func _init() -> void:
+	Globals.load_vehicle(Globals.car_id)
+
+func _ready() -> void:
+	vehicle = Globals.get_vehicle()
+	var _error = Globals.connect("new_max_speed", hud, "update_speed")
+	if vehicle:
+		vehicle.position = SPAWN_POINT
+	else: 
+		vehicle = load("res://Project/Vehicles/RedCar/Redcar.tscn").instance()
+		vehicle.position = SPAWN_POINT
+	call_deferred("add_child", vehicle)
+
+func _process(_delta) -> void:
+	var car_velocity = (vehicle.linear_velocity.x + vehicle.linear_velocity.y) /2
+	Globals.set_max_speed(car_velocity)
 
 func _on_FinishZone_body_entered(body):
-	queue_free()
+	if body == vehicle:
+		Globals.disconnect("new_max_speed", hud, "update_speed")
+		Globals.determine_current_level(get_tree().current_scene.filename)
+		transition.transition_to("res://Project/UI/End/End.tscn")
