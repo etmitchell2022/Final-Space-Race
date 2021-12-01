794e3b44f7510d6a47af4895cdf5985d6acd2ed2
Evan Mitchell <etmitchell@bsu.edu>
2021-12-01 17:11:39 -0500

    Remove unused code

--- Project/Common/Coin.gd
+++ Project/Common/Coin.gd
@@ -2,12 +2,6 @@ extends Node2D
 
 onready var coin = $Sprite
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _on_CoinZone_body_entered(_body) -> void:
-	print("hit")
 	Globals.coin_score += 10
 	coin.queue_free()
-	print(Globals.coin_score)
