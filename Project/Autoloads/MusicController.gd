9e8c6348cd5e0d9ed6e898a5706660a539a3eedc
Spencer Naugler <spencernaugler7@gmail.com>
2021-12-02 10:21:06 -0500

    rename root directory

--- Project/Autoloads/MusicController.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var game_music := load("res://Project/Common/backgroundMusic.ogg")
-	
-func play_music() -> void:
-	$Music.stream = game_music
-	$Music.play(0)
---
+++ SpaceRace/Vehicles/Truck/Truck.gd
@@ -0,0 +1,15 @@
+extends "res://SpaceRace/Vehicles/Car.gd"
+
+onready var deathTimer = $DeathTimer
+var crashes = 0
+
+func _on_CrashZone_body_entered(body) -> void:
+	if crashes == 1:
+		if body != RigidBody2D:
+			._explosion()
+			deathTimer.start()
+	crashes += 1
+
+func _on_DeathTimer_timeout() -> void:
+	Globals.determine_current_level(get_tree().current_scene.filename)
+	var _ignored := get_tree().change_scene("res://SpaceRace/UI/End/End.tscn")
